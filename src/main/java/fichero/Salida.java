40a78989fa82f7c0389a3cd897002025787391e3
GuillermoHRT <guillermohrt@gmail.com>
2018-03-27 12:27:06 +0200

    Salida empezado

--- src/main/java/fichero/Salida.java
+++ src/main/java/fichero/Salida.java
@@ -1,8 +1,15 @@
 package fichero;
 
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 /**
  * Created by al361891 on 20/03/18.
  */
-public class Salida extends Fichero {
-
+public class Salida extends Fichero implements Serializable{
+    FileOutputStream fos = new FileOutputStream("datos.bin");
+    ObjectOutputStream oos = new ObjectOutputStream(fos);
+    oos.writeObject(gestor.Metodos metodos);
+    oos.close();
 }
