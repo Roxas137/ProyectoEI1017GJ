611fd97f3da52d2ce359ede87de25f919bdeb3e9
Roxas137 <36661014+Roxas137@users.noreply.github.com>
2018-03-27 12:32:07 +0200

    salida en proceso

--- src/main/java/fichero/Salida.java
+++ src/main/java/fichero/Salida.java
@@ -8,8 +8,10 @@ import java.io.Serializable;
  * Created by al361891 on 20/03/18.
  */
 public class Salida extends Fichero implements Serializable{
-    FileOutputStream fos = new FileOutputStream("datos.bin");
-    ObjectOutputStream oos = new ObjectOutputStream(fos);
-    oos.writeObject(gestor.Metodos metodos);
-    oos.close();
+    public void Salida() {
+        FileOutputStream fos = new FileOutputStream("datos.bin");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(gestor.Metodos metodos);
+        oos.close();
+    }
 }
